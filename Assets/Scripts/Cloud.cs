371b409f2c480cc8d656018add487a4556b7d22a
Tenshi <tenshii.tv@gmail.com>
2024-10-10 20:10:22 +0900

    add clouds

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    public int spawnX = 16;
+    public int minX = -70;
+
+    public float minSpeed = 0.5f;
+    public float maxSpeed = 2.0f;
+
+    private float speed = 0.0f;
+
+    void Start()
+    {
+        speed = Random.Range(minSpeed, maxSpeed);
+    }
+
+    void Update()
+    {
+        if (transform.position.x < minX)
+        {
+            transform.position = new Vector3(spawnX, transform.position.y, transform.position.z);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
