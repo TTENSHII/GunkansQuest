167a5eae2ac4f3c4937a8916e14acb7f9b467b6f
Tenshi <tenshii.tv@gmail.com>
2024-10-10 17:09:02 +0900

    add first map

--- Assets/Scripts/CameraMovements.cs
+++ Assets/Scripts/CameraMovements.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class CameraMovements : MonoBehaviour
 {
     private GameObject player;
     private float yOffset = 2.0f;
