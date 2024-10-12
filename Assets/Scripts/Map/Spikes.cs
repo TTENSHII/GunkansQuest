94cd243c7cd703f868bb364b079037eadd297fb7
Tenshi <tenshii.tv@gmail.com>
2024-10-12 14:50:59 +0900

    add spikes

---
+++ Assets/Scripts/Map/Spikes.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    public int damage = 1;
+
+    private AudioSource audioSource = null;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<PlayerLife>().TakeDamage(damage);
+            audioSource.Play();
+        }
+        if (collision.CompareTag("Enemy"))
+        {
+            collision.GetComponent<Enemies>().ReceiveDamage(10);
+            audioSource.Play();
+        }
+    }
+}
