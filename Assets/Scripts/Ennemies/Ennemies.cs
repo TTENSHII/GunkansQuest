46d8c8eb53c097a5e7fee5ade6b806efe2135d94
Tenshi <tenshii.tv@gmail.com>
2024-10-13 15:51:28 +0900

    refactor ennemies classes

--- Assets/Scripts/Ennemies/Ennemies.cs
+++ Assets/Scripts/Ennemies/Ennemies.cs
@@ -2,16 +2,139 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum EnnnemiesType
+public abstract class Enemies : MonoBehaviour
 {
-    Crabby
-}
+    protected int maxHealth = 3;
+    protected int health = 3;
+    protected int attackDamage = 1;
+    protected float speed = 5.0f;
+    protected float attackRange = 1.8f;
+    protected float attackCountdown = 0.5f;
 
-public interface Enemies
-{
-    public int GetAttackDamage();
-    public void ReceiveDamage(int damage);
-    public void Die();
-    public void Move();
-    public void Attack();
+    protected bool isStunned = false;
+    protected bool canAttack = true;
+
+    protected Animator animator = null;
+    protected Rigidbody2D rb = null;
+    protected PlayerMovements player = null;
+    protected PlayerLife playerLife = null;
+    protected FloatingHealthBar healthBar = null;
+
+    protected virtual void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<PlayerMovements>();
+        playerLife = FindObjectOfType<PlayerLife>();
+        healthBar = GetComponentInChildren<FloatingHealthBar>();
+        healthBar.UpdateHealthBar(health, maxHealth);
+    }
+
+    protected bool IsPlayerInRange()
+    {
+        if (player == null) return false;
+
+        float distanceX = Mathf.Abs(player.transform.position.x - transform.position.x);
+        float distanceY = Mathf.Abs(player.transform.position.y - transform.position.y);
+
+        if (distanceX <= attackRange && distanceY <= 1.0f)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected void Update()
+    {
+        if (IsPlayerInRange())
+        {
+            Attack();
+        }
+    }
+
+    private void ResetAttackPossibility()
+    {
+        canAttack = true;
+    }
+
+    protected void Attack()
+    {
+        if (isStunned) return;
+        if (!canAttack) return;
+        animator.SetBool("IsAttacking", true);
+        canAttack = false;
+        Invoke("ResetAttackPossibility", attackCountdown);
+    }
+
+    protected void FixedUpdate()
+    {
+        if (health <= 0)
+        {
+            animator.SetFloat("Speed", 0);
+            return;
+        }
+        Move();
+        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+    }
+
+    protected void Move()
+    {
+        if (isStunned || player == null) return;
+
+        float distanceX = Mathf.Abs(player.transform.position.x - transform.position.x);
+
+        if (distanceX > 8)
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            return;
+        }
+
+        if (player.transform.position.x > transform.position.x)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+    }
+
+    protected void OnAttackAnimationEnd()
+    {
+        animator.SetBool("IsAttacking", false);
+    }
+
+    protected void OnHitAnimationEnd()
+    {
+        isStunned = false;
+        animator.SetBool("IsTakingDamage", false);
+    }
+
+    public void ReceiveDamage(int damage)
+    {
+        health -= damage;
+        isStunned = true;
+        rb.velocity = Vector2.zero;
+        healthBar.UpdateHealthBar(health, maxHealth);
+        animator.SetBool("IsTakingDamage", true);
+        if (health <= 0)
+        {
+            animator.SetBool("IsDead", true);
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    protected void OnDieAnimationEnd()
+    {
+       Destroy(gameObject);
+    }
+
+    protected void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerLife.TakeDamage(attackDamage);
+        }
+    }
 }
--- Assets/Scripts/Ennemies/FierceTooth.cs
+++ Assets/Scripts/Ennemies/FierceTooth.cs
@@ -2,151 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FierceeTooth : MonoBehaviour, Enemies
+public class FierceeTooth : Enemies
 {
-    public int maxHealth = 3;
-    public int health = 3;
-    public float speed = 5.0f;
-    public int attackDamage = 1;
-    public float attackRange = 1.8f;
-    public float attackCountdown = 0.5f;
-
-    private bool isStunned = false;
-    private bool canAttack = true;
-
-    private Animator animator = null;
-    private Rigidbody2D rb = null;
-    private PlayerMovements player = null;
-    private PlayerLife playerLife = null;
-
-    [SerializeField] FloatingHealthBar healthBar = null;
-
-    void Start()
+    protected override void Start()
     {
-        animator = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(-speed, 0);
-        player = FindObjectOfType<PlayerMovements>();
-        playerLife = FindObjectOfType<PlayerLife>();
-        healthBar = GetComponentInChildren<FloatingHealthBar>();
-        healthBar.UpdateHealthBar(health, maxHealth);
-    }
+        base.Start();
 
-    private bool IsPlayerInRange()
-    {
-        if (player == null) return false;
-        float playerX = player.transform.position.x;
-        float playerY = player.transform.position.y;
-        float crabbyX = transform.position.x;
-        float crabbyY = transform.position.y;
-        float distanceX = Mathf.Abs(playerX - crabbyX);
-        float distanceY = Mathf.Abs(playerY - crabbyY);
-        if (distanceX <= attackRange && distanceY <= 1.0f)
-        {
-            return true;
-        }
-        return false;
-    }
-
-    void Update()
-    {
-        if (IsPlayerInRange())
-        {
-            Attack();
-        }
-    }
-
-    void FixedUpdate()
-    {
-        Move();
-        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
-    }
-
-    public int GetAttackDamage()
-    {
-        return attackDamage;
-    }
-
-    public void ReceiveDamage(int damage)
-    {
-        health -= damage;
-        isStunned = true;
-        rb.velocity = Vector2.zero;
-        healthBar.UpdateHealthBar(health, maxHealth);
-        animator.SetBool("IsTakingDamage", true);
-        if (health <= 0)
-        {
-            isStunned = true;
-            animator.SetBool("IsDead", true);
-        }
-    }
-
-    public void Die()
-    {
-        Destroy(gameObject);
-    }
-
-    private void OnDieAnimationEnd()
-    {
-       Die();
-    }
-
-    private void OnAttackAnimationEnd()
-    {
-        animator.SetBool("IsAttacking", false);
-    }
-
-    private void OnHitAnimationEnd()
-    {
-        isStunned = false;
-        animator.SetBool("IsTakingDamage", false);
-        Move();
-    }
-
-    public void Move()
-    {
-        if (isStunned) return;
-        if (player == null) return;
-        float playerX = player.transform.position.x;
-        float crabbyX = transform.position.x;
-
-        float distanceX = Mathf.Abs(playerX - crabbyX);
-
-        if (distanceX > 8)
-        {
-            rb.velocity = new Vector2(0, rb.velocity.y);
-            return;
-        }
-
-        if (playerX > crabbyX)
-        {
-            rb.velocity = new Vector2(speed, rb.velocity.y);
-        }
-        else
-        {
-            rb.velocity = new Vector2(-speed, rb.velocity.y);
-        }
-    }
-
-    private void ResetAttackPossibility()
-    {
-        canAttack = true;
-    }
-
-    public void Attack()
-    {
-        if (isStunned) return;
-        if (!canAttack) return;
-        animator.SetBool("IsAttacking", true);
-        canAttack = false;
-        Invoke("ResetAttackPossibility", attackCountdown);
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            playerLife.TakeDamage(attackDamage);
-        }
+        maxHealth = 4;
+        health = maxHealth;
+        speed = 2;
+        attackDamage = 1;
+        attackRange = 2;
+        attackCountdown = 0.5f;
     }
 }
