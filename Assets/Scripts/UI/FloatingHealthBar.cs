191d9396c02234e665d1ed2e9b184f3c87c86246
Tenshi <tenshii.tv@gmail.com>
2024-10-13 12:30:28 +0900

    fix health bar

--- Assets/Scripts/UI/FloatingHealthBar.cs
+++ Assets/Scripts/UI/FloatingHealthBar.cs
@@ -6,9 +6,14 @@ using UnityEngine.UI;
 public class FloatingHealthBar : MonoBehaviour
 {
     [SerializeField] private Slider slider = null;
+    [SerializeField] private Image fillImage = null;
 
     public void UpdateHealthBar(int currentHealth, int maxHealth)
     {
         slider.value = (float)currentHealth / maxHealth;
+        if (fillImage != null && slider.value <= 0)
+        {
+            fillImage.color = new Color(fillImage.color.r, fillImage.color.g, fillImage.color.b, 0);
+        }
     }
 }
